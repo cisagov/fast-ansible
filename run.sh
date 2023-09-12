72e00d6d682f9530414c912cf3b937bc280d0a7f
jdcisa <joseph.doherty@associates.cisa.dhs.gov>
2023-09-12 09:35:24 -0400

    added run.sh script and started working on firefox stuff

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [[ "$#" -eq 1 && [[ "$1" == "local" || "$1" == "remote" ]] ]] ; then
+
+	sudo apt update
+	sudo apt -y install ansible
+
+	if [[ "$1" -eq "local" ]] ; then
+		ansible-playbook site.yaml --connection=local -i local_only_inventory.yaml -e ansible_python_interpreter=/usr/bin/python3
+	elif [[ "$2" -eq "remote" ]] ; then
+		ansible-playbook site.yaml -i standard_inventory.yaml -e ansible_python_interpreter=/usr/bin/python3
+	fi
+
+else
+
+	echo '"./run.sh local" or "./run.sh remote"'
+fi
